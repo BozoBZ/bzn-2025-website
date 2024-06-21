07fe455b20cc6c225bf4ae12b622b98bd603dfb0
BozoBZ <dtaraboi@outlook.it>
2024-06-21 13:12:38 +0000

    Retrieving data from list variable

--- app.py
+++ app.py
@@ -2,9 +2,40 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'vorname': 'Pietro',
+        'nachname': 'Bonelli',
+        'ge': 'M',
+        'geburtsdatum': '10.10.2006'
+    },
+    {
+        'id': 2,
+        'vorname': 'Marina',
+        'nachname': 'Badstübner',
+        'ge': 'W',
+        'geburtsdatum': '10.11.2013'
+    },
+{
+    'id': 3,
+    'vorname': 'Jascha',
+    'nachname': 'Luft',
+    'ge': 'M',
+    'geburtsdatum': '01.10.2011'
+},
+{
+    'id': 4,
+    'vorname': 'Liz',
+    'nachname': 'Brunner',
+    'ge': 'M',
+    'geburtsdatum': '12.12.2012'
+}
+]
+
 @app.route("/")
 def hello_world():
-    return render_template ("home.html")
+    return render_template ("home.html", jobs=JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
